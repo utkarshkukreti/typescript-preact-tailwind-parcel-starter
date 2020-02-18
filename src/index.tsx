66227fcab404b93ef769b3b1d066c84156951b73
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-02-18 11:17:31 +0530

    better animation

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ const Button = ({ text }: { text: string }) => {
   const [count, setCount] = useState(0);
   return (
     <button
-      class="px-6 sm:px-10 py-5 bg-white font-semibold text-purple-600 sm:text-lg rounded-full shadow-lg focus:outline-none transition duration-150 transform hover:-translate-y-1 hover:bg-gray-300"
+      class="px-6 sm:px-10 py-5 bg-white font-semibold text-purple-600 sm:text-lg rounded-full shadow-lg focus:outline-none transition duration-150 transform hover:-translate-y-1 hover:bg-gray-300 hover:scale-105"
       onClick={() => setCount(count + 1)}
     >
       {text.replace(/{}/g, `${count}`)}
